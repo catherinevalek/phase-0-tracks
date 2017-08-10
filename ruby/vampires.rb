85032f6775d4c98f8bc753ba945842c3448bf13b
Catherine Valek <catherine.valek@gmail.com>
2017-08-09 23:50:40 -0500

    Create conditions to determine if employee is vampire

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,19 +1,57 @@
 puts "What is your name?"
 vamp_name = gets.chomp
-p vamp_name
 
 puts "How old are you?"
-age = gets.chomp
-p age
+age = gets.chomp.to_i
 
 puts "What year were you born?"
-birth_year = gets.chomp
-p birth_year
+birth_year = gets.chomp.to_i
 
-puts "Should we order garlic break for you?"
+puts "Should we order garlic break for you? (y/n)"
 garlic_decision = gets.chomp
-p garlic_decision
 
-puts "Would you like to enroll in the company's health insurance?"
+puts "Would you like to enroll in the company's health insurance? (y/n)"
 insurance_decision = gets.chomp
-p insurance_decision
+
+
+#determine if employee age is correct
+if (age == (2017 - birth_year)) || (age == (2016 - birth_year))
+	age_correct = true
+else
+	age_correct = false
+end
+
+#determine if employee eats garlic
+if garlic_decision == "y"
+	eats_garlic = true
+else 
+	eats_garlic = false
+end
+
+#determine if employee wants insurance
+if insurance_decision == "y"
+	wants_insurance = true
+else 
+	wants_insurance = false
+end
+
+
+vampire_result = true
+	
+if vamp_name == "Drake Cula" || vamp_name == "Tu Fang"
+	p "Definitely a vampire"
+elsif age_correct == false && eats_garlic == false && wants_insurance == false
+	p "Almost certainy a vampire"
+
+elsif age_correct == true && (eats_garlic || wants_insurance)
+	p "Probably not a vampire"
+elsif age_correct == false && (eats_garlic == fals|| !wants_insurance)
+	p "Probably a vampire"
+
+else
+	p "Results inconclusive"
+end
+
+
+
+
