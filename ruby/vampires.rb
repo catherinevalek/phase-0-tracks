917bc26e12cc701230f2eb586116a5683d4ba91b
Catherine Valek <catherine.valek@gmail.com>
2017-08-09 21:48:55 -0500

    Prompt user for data and store it

---
+++ ruby/vampires.rb
@@ -0,0 +1,19 @@
+puts "What is your name?"
+vamp_name = gets.chomp
+p vamp_name
+
+puts "How old are you?"
+age = gets.chomp
+p age
+
+puts "What year were you born?"
+birth_year = gets.chomp
+p birth_year
+
+puts "Should we order garlic break for you?"
+garlic_decision = gets.chomp
+p garlic_decision
+
+puts "Would you like to enroll in the company's health insurance?"
+insurance_decision = gets.chomp
+p insurance_decision
