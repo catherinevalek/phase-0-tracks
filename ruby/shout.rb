442f1d8a7597a6f8a04cbd27de76084e3e472a8d
Catherine Valek <catherine.valek@gmail.com>
2017-08-23 14:23:21 -0500

    Add two classes that call shout module methods

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,14 +1,42 @@
-module Shout
+# module Shout
   
-  def self.yell_angrily(words)
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+
+#   def self.yell_happily(words)
+#     words + "!!!" + " :)"
+#   end
+
+# end
+
+# p Shout.yell_angrily('wow')
+# p Shout.yell_happily('wow')
+
+module Shout
+  def yell_angrily(words)
     words + "!!!" + " :("
   end
 
-  def self.yell_happily(words)
+  def yell_happily(words)
     words + "!!!" + " :)"
   end
-
 end
 
-p Shout.yell_angrily('wow')
-p Shout.yell_happily('wow')
+class Child
+  include Shout
+end
+
+class Sports_fan
+  include Shout
+end
+
+tom = Child.new
+p tom.yell_angrily("I'm hungry")
+
+joe = Sports_fan.new
+p joe.yell_happily("gooaalllll")
+
+
+
+
