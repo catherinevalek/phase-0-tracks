7cbd0532266dc16f65c7478389c5c698e11f4e60
Catherine Valek <catherine.valek@gmail.com>
2017-09-01 13:00:08 -0500

    Refactor methods and add reflection

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -1,8 +1,8 @@
 
 # Virus Predictor
 
-# I worked on this challenge [by myself, with: ].
-# We spent [#] hours on this challenge.
+# I worked on this challenge with Nikita.
+# We spent 1.5 hours on this challenge.
 
 # EXPLANATION OF require_relative
 # require_relative allows us to access a file in the same
@@ -29,16 +29,17 @@ class VirusPredictor
 # and generates a number of deaths
   def predicted_deaths
     # predicted deaths is solely based on population density
-    if @population_density >= 200
-      number_of_deaths = (@population * 0.4).floor
-    elsif @population_density >= 150
-      number_of_deaths = (@population * 0.3).floor
-    elsif @population_density >= 100
-      number_of_deaths = (@population * 0.2).floor
-    elsif @population_density >= 50
-      number_of_deaths = (@population * 0.1).floor
-    else
+    case @population_density
+    when 0...50
       number_of_deaths = (@population * 0.05).floor
+    when 50...100
+      number_of_deaths = (@population * 0.1).floor
+    when 100...150
+      number_of_deaths = (@population * 0.2).floor
+    when 150...200
+      number_of_deaths = (@population * 0.3).floor
+    else
+      number_of_deaths = (@population * 0.4).floor
     end
 
     print "#{@state} will lose #{number_of_deaths} people in this outbreak"
@@ -48,21 +49,19 @@ class VirusPredictor
   def speed_of_spread #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
-    speed = 0.0
-
-    if @population_density >= 200
-      speed += 0.5
+    months = if @population_density >= 200
+      0.5
     elsif @population_density >= 150
-      speed += 1
+      1
     elsif @population_density >= 100
-      speed += 1.5
+      1.5
     elsif @population_density >= 50
-      speed += 2
+      2
     else
-      speed += 2.5
+      2.5
     end
 
-    puts " and will spread across the state in #{speed} months.\n\n"
+    puts " and will spread across the state in #{months} months.\n\n"
 
   end
 
@@ -92,12 +91,23 @@ STATE_DATA.each do |state, data|
 end
 
 
-
-
-
-
-
-
-
 #=======================================================================
 # Reflection Section
+
+# What are the differences between the two different hash syntaxes shown in 
+# the state_data file?
+# One uses has rockets and one uses symbols. Hash rockets can facilitate strings
+# as keys. 
+# What does require_relative do? How is it different from require?
+# Require_relative allows us to access a file in the same
+# directory without having to copy the direct code.
+# Require looks through ruby gems to see if a certain library is present.
+# What are some ways to iterate through a hash?
+# You can iterate with .each.
+# When refactoring virus_effects, what stood out to you about the variables, if anything?
+# It wasn't necessary to have the instance variables as arguments, because they are
+# accessible in all the methods in the class. 
+# What concept did you most solidify in this challenge?
+# This solidified when and how to use attributes. 
+
+
