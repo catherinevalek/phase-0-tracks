ba1bcaa4ea36dc9b933960df7d0f8ce1d34764f8
Catherine Valek <catherine.valek@gmail.com>
2017-09-01 14:42:16 -0500

    Create a function to determine if key-value pair is in two objects

--- js/algos.js
+++ js/algos.js
@@ -1,14 +1,3 @@
-// In algos.js, write a function that takes an array of words 
-// or phrases and returns the longest word or phrase in the array. 
-// So if we gave your function the array of ["long phrase","longest 
-// phrase","longer phrase"], it would return "longest phrase". 
-// This is a logical reasoning challenge, so try to reason through 
-// this based on what you already know, rather than Googling the 
-// fanciest solution or looking up built-in sorting functions. 
-// Note that "some string".length will do what you expect in JS.
-// Add driver code that tests your function on a few arrays.
-
-
 // For each item in the array
 //   Find the length of the word or phrase
 //     Compare the length to the next item in the array
@@ -23,9 +12,47 @@ function find_longest_word(word_list) {
       longest_item = word_list[i];
     }
   }
-  console.log(longest_item);
+  console.log(longest_item)
 }
 
-var words_and_phrases = ["long phrase","longest phrase","longer phrase"];
 
+function key_value_match(first_object, second_object) {
+  for (var key_from_first in first_object) {
+  	for (var key_from_second in second_object) {
+  	  if ((key_from_first == key_from_second) && (first_object[key_from_first] == second_object[key_from_second]))
+  	    return true
+  	}
+  }
+  return false
+}
+
+
+
+
+var words_and_phrases = ["long phrase","longest phrase","longer phrase"];
 find_longest_word(words_and_phrases);
+
+var more_words = ["elephant","penguin","tiger"];
+find_longest_word(more_words);
+
+var first = {name: 'Spot', 'age': 3, isGoodDog: true};
+var second = {name: 'Joe', 'age': 3, isGoodDog: false};
+console.log(key_value_match(first, second))
+
+var third = {name: 'Tome', 'age': 4, isGoodDog: true};
+var fourth = {name: 'Joe', 'age': 3, isGoodDog: true};
+console.log(key_value_match(third, fourth))
+
+var fifth = {name: 'Tome', 'age': 4, isGoodDog: false};
+var sixth = {name: 'Joe', 'age': 3, isGoodDog: true};
+console.log(key_value_match(fifth, sixth))
+
+var seventh = {name: 'Tome', 'age': 4, isGoodDog: false};
+var eighth = {color: 'red', 'shape': 'square', size: 'small'};
+console.log(key_value_match(seventh, eighth))
+
+
+
+
+
+
